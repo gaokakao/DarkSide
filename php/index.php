8895de031b12309b95cc9095a41f995e695cdc9c
gaokakao <giedriuskk@gmail.com>
2024-09-06 16:12:14 +0300

    update

--- php/index.php
+++ php/index.php
@@ -9,9 +9,9 @@ $ip = $_GET['ip'] ?? '0';
 $timestamp = date('Y-m-d H:i:s');
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {     die("Connection failed: " . $conn->connect_error); }
-$sql = "INSERT INTO gps (latitude, longitude, ip, timestamp) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE latitude=?, longitude=?";
+$sql = "INSERT INTO gps (latitude, longitude, ip) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE latitude=?, longitude=?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sssssss", $latitude, $longitude, $ip, $timestamp, $latitude, $longitude);
+$stmt->bind_param("sssssss", $latitude, $longitude, $ip, $latitude, $longitude);
 $stmt->execute();
 echo "OK";
 ?>
