dadfdda0cfbeeedddfd43c668988cef51f2f5d7b
gaokakao <giedriuskk@gmail.com>
2024-09-12 22:41:52 +0300

    enter username working

--- app/src/main/java/com/gaokakao/darkside/MainActivity.java
+++ app/src/main/java/com/gaokakao/darkside/MainActivity.java
@@ -1,17 +1,16 @@
 // MainActivity.java
 package com.gaokakao.darkside;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.View;  // Import View class
 import android.widget.EditText;
 import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText usernameEditText;
-    private Button submitButton;
     private TextView usernameTextView;
     private SharedPreferences sharedPreferences;
     private static final String PREFS_NAME = "user_prefs";
@@ -21,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        usernameEditText = findViewById(R.id.usernameEditText);
-        submitButton = findViewById(R.id.submitButton);
         usernameTextView = findViewById(R.id.usernameTextView);
         sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         String savedUsername = sharedPreferences.getString(USERNAME_KEY, null);
@@ -34,21 +31,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void promptForUsername() {
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Username");
+        final EditText input = new EditText(this);
+        input.setHint("Username");
+        input.setTextColor(getResources().getColor(android.R.color.holo_blue_light));
+        input.setHintTextColor(getResources().getColor(android.R.color.holo_orange_light));
+        builder.setView(input);
+        builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String username = usernameEditText.getText().toString();
+            public void onClick(DialogInterface dialog, int which) {
+                String username = input.getText().toString();
                 if (!username.isEmpty()) {
                     sharedPreferences.edit().putString(USERNAME_KEY, username).apply();
                     displayUsername(username);
                 }
             }
         });
+        builder.setCancelable(false);
+        builder.show();
     }
 
     private void displayUsername(String username) {
-        usernameEditText.setVisibility(View.GONE);
-        submitButton.setVisibility(View.GONE);
         usernameTextView.setText(username);
         usernameTextView.setVisibility(View.VISIBLE);
     }
