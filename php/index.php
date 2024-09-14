af74eae26b569ad188f9b1cc398cef44f5d395fc
gaokakao <giedriuskk@gmail.com>
2024-09-14 15:44:37 +0300

    update

--- php/index.php
+++ php/index.php
@@ -7,16 +7,21 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$latitude = $_GET['latitude'];
-$longitude = $_GET['longitude'];
-$user = $_GET['user'];
-$sql = "INSERT IGNORE INTO gps (latitude, longitude, user) VALUES ('$latitude', '$longitude', '$user')";
-
-$result = $conn->query($sql);
-if ($result === TRUE) {
-    echo "New record created successfully! ok";
+$latitude = isset($_GET['latitude']) ? $_GET['latitude'] : empty;
+$longitude = isset($_GET['longitude']) ? $_GET['longitude'] : empty;
+$user = isset($_GET['user']) ? $_GET['user'] : empty;
+if ($latitude !== null && $longitude !== null && $user !== null) {
+    $latitude = $conn->real_escape_string($latitude);
+    $longitude = $conn->real_escape_string($longitude);
+    $user = $conn->real_escape_string($user);
+    $sql = "INSERT IGNORE INTO gps (latitude, longitude, user) VALUES ('$latitude', '$longitude', '$user')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully! ok";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Missing parameters";
 }
 $conn->close();
 ?>
