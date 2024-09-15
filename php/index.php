6e83bd2617adfe01fc5cd7af4d1fa36d00c14b90
gaokakao <giedriuskk@gmail.com>
2024-09-15 17:18:52 +0300

    update

--- php/index.php
+++ php/index.php
@@ -3,23 +3,17 @@ $servername = "localhost";
 $username = "user";
 $password = "user";
 $dbname = "gps";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $latitude = isset($_GET['latitude']) ? $_GET['latitude'] : '';
 $longitude = isset($_GET['longitude']) ? $_GET['longitude'] : '';
 $user = isset($_GET['user']) ? $_GET['user'] : '';
-
 $sql = "INSERT INTO gps (latitude, longitude, user) VALUES ('$latitude', '$longitude', '$user') ON DUPLICATE KEY UPDATE latitude='$latitude', longitude='$longitude'";
-
 if ($conn->query($sql) === TRUE) {
-    echo "Record inserted or updated successfully! ok";
+    echo "OK";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "BAD: " . $sql . "<br>" . $conn->error;
 }
-
 $conn->close();
