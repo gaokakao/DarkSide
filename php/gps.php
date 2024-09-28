c9cd71713e78ceed1b137bc25cb5a5d3f293cee7
gaokakao <giedriuskk@gmail.com>
2024-09-28 09:46:09 +0300

    new update

--- php/gps.php
+++ php/gps.php
@@ -7,20 +7,20 @@ function calculateDistance($lat1, $lon1, $lat2, $lon2)
     $a = sin($dLat / 2) ** 2 + cos($lat1 * M_PI / 180) * cos($lat2 * M_PI / 180) * sin($dLon / 2) ** 2;
     return $earthRadius * (2 * atan2(sqrt($a), sqrt(1 - $a)));
 }
-$sql = new mysqli("localhost", "user", "user", "gps");
+$sql = new mysqli(           "localhost", "user", "user", "gps");
 if ($sql->connect_error) { die("Connection failed: " . $sql->connect_error);}
 $latitude = isset($_GET['latitude']) ? (float)$_GET['latitude'] : 0;
 $longitude = isset($_GET['longitude']) ? (float)$_GET['longitude'] : 0;
 $user = isset($_GET['user']) ? $_GET['user'] : 'empty';
 
-$sql->query("DELETE FROM gps WHERE user = 'empty'");
+$sql->query("DELETE FROM gps WHERE user 'empty");
 $result = $sql->query("SELECT * FROM gps WHERE user='$user'");
 if ($result->num_rows > 0) {
     $sql->query("UPDATE gps SET latitude=$latitude, longitude=$longitude WHERE user='$user'");
 } else {
     $sql->query("INSERT INTO gps (latitude,longitude,user) VALUES ($latitude,$longitude,'$user')");
 }
-$sql->query("DELETE FROM gps WHERE user = empty");
+$sql->query("DELETE FROM gps WHERE user 'empty");
 $result = $sql->query("SELECT user,latitude,longitude FROM gps");
 $users = [];
 while ($row = $result->fetch_assoc()) {
